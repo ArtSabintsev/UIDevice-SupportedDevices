c62ad5695d5c63cf82a26c35fe20ef9c7cf2e057
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2013-10-28 10:03:00 -0400

    Added more newer model data

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-# UIDevice-SupportedDevices
+# UIDevice+SupportedDevices
 
 ### About
 An Objective-C category on `UIDevice` that returns devices based on the supportedDevices key in the JSON results returned in an [iTunes Lookup API call](http://itunes.apple.com/lookup).
 
-This category was born within [issue 35](https://github.com/ArtSabintsev/Harpy/issues/35) of the [Harpy](http://www.github.com/ArtSabintsev/Harpy) repository.
-
-### Changelog (v0.0.1)
-- Initial Code Commit. Do not download or use, as this project is a WIP.
+### Changelog (v0.0.2)
+- Added iPhone 4S `else-if` statement
+- Began populating 
 
 ### Installation Instructions
 
 - Coming Soon.
+
+### Inspired by
+This category was born within [issue 35](https://github.com/ArtSabintsev/Harpy/issues/35) of the [Harpy](http://www.github.com/ArtSabintsev/Harpy) repository. The underlying idea and some of the research for building this category came from [Aaron Brager](http://www.github.com/getaaron). 
 
 ### Created and maintained by
 - [Arthur Ariel Sabintsev](http://www.sabintsev.com/) 
--- UIDevice+SupportedDevices.m
+++ UIDevice+SupportedDevices.m
@@ -50,16 +50,17 @@
     else if ([machineName isEqualToString:@"iPod5,1"]) deviceName = @"";    // iPod touch 5G
     else if ([machineName isEqualToString:@"iPhone1,1"]) deviceName = @"";  // iPhone 2G GSM
     else if ([machineName isEqualToString:@"iPhone1,2"]) deviceName = @"";  // iPhone 3G GSM
-    else if ([machineName isEqualToString:@"iPhone2,1"]) deviceName = @"";  // iPhone 3GS GSM
+    else if ([machineName isEqualToString:@"iPhone2,1"]) deviceName = @"iPhone-3GS";  // iPhone 3GS GSM
     else if ([machineName isEqualToString:@"iPhone3,1"]) deviceName = @"";  // iPhone 4 GSM
     else if ([machineName isEqualToString:@"iPhone3,2"]) deviceName = @"";  // iPhone 4 GSM Rev A
     else if ([machineName isEqualToString:@"iPhone3,3"]) deviceName = @"";  // iPhone 4 CDMA
-    else if ([machineName isEqualToString:@"iPhone5,1"]) deviceName = @"";  // iPhone GSM
-    else if ([machineName isEqualToString:@"iPhone5,2"]) deviceName = @"";  // iPhone GSM+CDMA
-    else if ([machineName isEqualToString:@"iPhone5,3"]) deviceName = @"";  // iPhone GSM
-    else if ([machineName isEqualToString:@"iPhone5,4"]) deviceName = @"";  // iPhone Global
-    else if ([machineName isEqualToString:@"iPhone6,1"]) deviceName = @"";  // iPhone GSM
-    else if ([machineName isEqualToString:@"iPhone6,2"]) deviceName = @"";  // iPhone Global
+    else if ([machineName isEqualToString:@"iPhone4,1"]) deviceName = @"iPhone4s";  // iPhone 4S
+    else if ([machineName isEqualToString:@"iPhone5,1"]) deviceName = @"iPhone5";  // iPhone 5 GSM
+    else if ([machineName isEqualToString:@"iPhone5,2"]) deviceName = @"iPhone5";  // iPhone 5 GSM+CDMA
+    else if ([machineName isEqualToString:@"iPhone5,3"]) deviceName = @"";  // iPhone 5c GSM
+    else if ([machineName isEqualToString:@"iPhone5,4"]) deviceName = @"";  // iPhone 5c Global
+    else if ([machineName isEqualToString:@"iPhone6,1"]) deviceName = @"";  // iPhone 5s GSM
+    else if ([machineName isEqualToString:@"iPhone6,2"]) deviceName = @"";  // iPhone 5s Global
     else deviceName = @"Unknown";
     
     return deviceName;
